936c5c63fd1277291ca67f8780743abf404d696c
sajad Afkhami <sajad.afkhami10@gmail.com>
2025-05-25 10:33:25 +0330

    add Loader with css

---
+++ src/components/modules/Loader.jsx
@@ -0,0 +1,9 @@
+import styles from "./Loader.module.css";
+
+function Loader() {
+  return (
+    <div className={styles.container}><span className={styles.Loader}></span></div>
+  )
+}
+
+export default Loader
