52f7bb61d3654423864b175c2f0ae991c1bd765a
sajad Afkhami <sajad.afkhami10@gmail.com>
2025-05-25 10:31:31 +0330

    use queryClient & router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import AuthPage from "./pages/AuthPage";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {  
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: 1,
+        staleTime: 60 * 1000,
+      },
+  },
+});
   return (
-    <div>
-      <h1>پروژه دیوار</h1>
-      <AuthPage />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
   
 }
